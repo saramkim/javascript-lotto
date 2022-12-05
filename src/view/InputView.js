2fa0de3e50c757f76aeecfaaa0a2ce39bde2d26b
saram <yujun1018@naver.com>
2022-12-05 16:58:55 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -7,6 +7,10 @@ const InputView = {
       Validator.validatePurchaseAmount(input);
     });
   },
+
+  readWinningNumber() {
+    Console.readLine('당첨 번호를 입력해 주세요.', (input) => {});
+  },
 };
 
 module.exports = InputView;
